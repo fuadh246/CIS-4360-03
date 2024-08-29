1a61faf846e96e9a35eeecd080a50407e801f9e8
fuadh246 <fuadh246@gmail.com>
2024-08-29 16:33:26 -0400

    initial commit

---
+++ run_bisection_method.py
@@ -0,0 +1,47 @@
+'''
+@project       : Temple University CIS 4360 Computational Methods in Finance
+@Instructor    : Dr. Alex Pang
+
+@Student Name  : first last
+
+@Date          : 9/2024
+
+Using Bi-section method to solve algerbic equation
+
+'''
+
+import math
+
+from bisection_method import bisection
+
+def func1(x):
+    '''
+    function to solve equation the 2 x^3 - 5 x^2 + 1 = 0
+    '''
+    # TODO
+
+    # end TODO
+    return(result)
+
+def func2(x):
+    '''
+    function to solve the question sin(x) + x = 1
+    '''
+    # TODO
+
+    # end TODO
+    return(result)
+
+
+def run():
+
+    ''' TODO: 
+    call bisection method to solve the equation x^3 - 2 x^2 + 1 = 0
+    '''
+    '''
+    end TODO
+    '''
+
+
+if __name__ == "__main__":
+    run()
